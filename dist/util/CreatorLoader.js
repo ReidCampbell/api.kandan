67a5ab953fabfd333bcf322be48a71bfb1a9349a
ReidCampbell <reidscottcampbell@gmail.com>
2020-09-28 16:21:26 +0100

    Remove dist folder

--- dist/util/CreatorLoader.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCreatorLoader = void 0;
-exports.createCreatorLoader = () => new DataLoader();
-//# sourceMappingURL=CreatorLoader.js.map
