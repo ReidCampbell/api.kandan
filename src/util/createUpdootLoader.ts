6b407dc9d6a902b17420e255db65f4f845d5e5db
ReidCampbell <reidscottcampbell@gmail.com>
2020-10-04 14:14:29 +0100

    Create updoot loader

--- src/util/createUpdootLoader.ts
+++ src/util/createUpdootLoader.ts
@@ -2,19 +2,14 @@ import { Updoot } from '../entities/Updoot';
 import DataLoader from 'dataloader';
 
 export const createUpdootLoader = () =>
-  new DataLoader<
-    { postId?: number; commentId?: number; userId: number },
-    Updoot | null
-  >(async keys => {
-    const updoots = await Updoot.findByIds(keys as any);
-    const updootIdsToUpdoot: Record<string, Updoot> = {};
-    updoots.forEach(updoot => {
-      updootIdsToUpdoot[
-        `${updoot.userId}|${updoot.postId}|${updoot.commentId}`
-      ] = updoot;
-    });
+  new DataLoader<{ postId: number; userId: number }, Updoot | null>(
+    async keys => {
+      const updoots = await Updoot.findByIds(keys as any);
+      const updootIdsToUpdoot: Record<string, Updoot> = {};
+      updoots.forEach(updoot => {
+        updootIdsToUpdoot[`${updoot.userId}|${updoot.postId}`] = updoot;
+      });
 
-    return keys.map(
-      key => updootIdsToUpdoot[`${key.userId}|${key.postId}|${key.commentId}`]
-    );
-  });
+      return keys.map(key => updootIdsToUpdoot[`${key.userId}|${key.postId}`]);
+    }
+  );
